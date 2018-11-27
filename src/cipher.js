89e627efaa146c13ef3d1d2425311ee8ec42a43e
VeronicaManchola <veronica.manchola@hotmail.com>
2018-11-27 09:16:23 -0300

    ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,17 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+//window.cipher = {
+  //encode: () => {
+    let initialLetter = "A";
+    let offset = 7;
+
+    let letterToNumber = initialLetter.charCodeAt(0);
+    let newNumber = letterToNumber + offset;
+
+    let newLetter = String.fromCharCode(newNumber);
+
+    console.log(newLetter); 
+//}
+//};
+//   decode: () => {
+//     /* Acá va tu código */
+//   }
+// };
