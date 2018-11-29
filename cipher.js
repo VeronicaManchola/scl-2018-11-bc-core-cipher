92b9fc4a03f06ba06c8f1fbfd56af744aa6117c4
VeronicaManchola <veronica.manchola@hotmail.com>
2018-11-29 12:02:03 -0300

    cifra mayusculas, minusculas y offset negativo

--- cipher.js
+++ cipher.js
@@ -1,17 +1,68 @@
-//window.cipher = {
-  //encode: () => {
-    let initialLetter = "A";
-    let offset = 7;
+window.cipher = {
+  encode: (boxToCipher,offset) => {
+    
+    let resultCipher="";
 
-    let letterToNumber = initialLetter.charCodeAt(0);
-    let newNumber = letterToNumber + offset;
+    for(let i=0; i<boxToCipher.length; i++){
+      
+      letterToNumberCipher = boxToCipher.charCodeAt(i);
+      if(letterToNumberCipher === 32){
+        newLetterCipher = String.fromCharCode(letterToNumberCipher);
+        resultCipher += newLetterCipher
+      }else if(letterToNumberCipher >= 65 && letterToNumberCipher <= 90){
+        while (offset < 0){
+          offset = offset + 26
+        }
+        newNumberCipher = ((letterToNumberCipher - 65 + offset) % 26 + 65);
+        newLetterCipher = String.fromCharCode(newNumberCipher);
+        resultCipher += newLetterCipher
+      }else if(letterToNumberCipher >= 97 && letterToNumberCipher <= 122){
+        while (offset < 0){
+          offset = offset + 26
+        }
+        newNumberCipher = ((letterToNumberCipher - 97 + offset) % 26 + 97);
+        newLetterCipher = String.fromCharCode(newNumberCipher);
+        resultCipher += newLetterCipher
+      }
+    }
+    return resultCipher;
+  },
+    
+  decode: (boxToDecipher,offset) => {
+    
+    let resultDecipher="";
 
-    let newLetter = String.fromCharCode(newNumber);
+    for(let i=0; i<boxToDecipher.length; i++){
 
-    console.log(newLetter); 
-//}
-//};
-//   decode: () => {
-//     /* Acá va tu código */
-//   }
-// };
+      letterToNumberDecipher = boxToDecipher.charCodeAt(i);
+      if(letterToNumberDecipher === 32){
+        newLetterDecipher = String.fromCharCode(letterToNumberDecipher);
+        resultDecipher += newLetterDecipher
+      }else if(letterToNumberDecipher >= 65 && letterToNumberDecipher <= 90){
+        while (offset < 0){
+          offset = offset + 26
+        }
+        substractionResultUpperCase = (letterToNumberDecipher - 65 - offset)
+        while (substractionResultUpperCase < 0){
+          substractionResultUpperCase = substractionResultUpperCase + 26
+        }
+        newNumberDecipher = substractionResultUpperCase % 26 + 65
+        newLetterDecipher = String.fromCharCode(newNumberDecipher);
+        resultDecipher += newLetterDecipher
+        
+      }else if(letterToNumberDecipher >= 97 && letterToNumberDecipher <= 122){
+        while (offset < 0){
+          offset = offset + 26
+        }
+        substractionResultLowerCase = (letterToNumberDecipher - 97 - offset)
+        while (substractionResultLowerCase < 0){
+          substractionResultLowerCase = substractionResultLowerCase + 26
+        }
+        newNumberDecipher = substractionResultLowerCase % 26 + 97;
+        newLetterDecipher = String.fromCharCode(newNumberDecipher);
+        resultDecipher += newLetterDecipher
+      }
+    }
+    return resultDecipher;
+  }
+};
