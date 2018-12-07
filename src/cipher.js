ab0041e224fb302244df7df1aef60586f47ab5f7
VeronicaManchola <veronica.manchola@hotmail.com>
2018-12-06 23:02:12 -0300

    añadir comentarios y readme nuevo

--- src/cipher.js
+++ src/cipher.js
@@ -5,37 +5,55 @@ window.cipher = {
     let newLetterCipher;
     let resultCipher="";
 
+    //Recorre la longitud del string dado
     for(let i=0; i<boxToCipher.length; i++){
       
-
+      //Verifica si el caracter es ñ o Ñ, y de ser asi, lo agrega al resultado
       if(boxToCipher === "ñ" || boxToCipher === "Ñ"){
         resultCipher += boxToCipher
       }
+      //Verifica si el offset es negativo, de ser asi, le agrega 26 hasta ser positivo
       while (offset < 0){
         offset = offset + 26
       }
+      //Convierte la letra a número ASCII
       letterToNumberCipher = boxToCipher.charCodeAt(i);
+      /*Cifra espacios. Verifica si el número es 32, de ser asi, lo convierte a espacio de nuevo 
+      y lo agrega al resultado*/
       if(letterToNumberCipher === 32){
         newLetterCipher = String.fromCharCode(letterToNumberCipher);
         resultCipher += newLetterCipher
-      }else if(letterToNumberCipher >= 65 && letterToNumberCipher <= 90){
+      }
+      /*Cifra mayúsculas. Confirma si el numero ASCII esta entre 65 y 90. Completa la formula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a letra para luego agregarlo al resultado*/
+      else if(letterToNumberCipher >= 65 && letterToNumberCipher <= 90){
         newNumberCipher = ((letterToNumberCipher - 65 + offset) % 26 + 65);
         newLetterCipher = String.fromCharCode(newNumberCipher);
         resultCipher += newLetterCipher
-      }else if(letterToNumberCipher >= 97 && letterToNumberCipher <= 122){
+      }
+      /*Cifra minúsculas. Confirma si el numero ASCII esta entre 97 y 122. Completa la formula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a letra para luego agregarlo al resultado*/
+      else if(letterToNumberCipher >= 97 && letterToNumberCipher <= 122){
         newNumberCipher = ((letterToNumberCipher - 97 + offset) % 26 + 97);
         newLetterCipher = String.fromCharCode(newNumberCipher);
         resultCipher += newLetterCipher
-      }else if(letterToNumberCipher >= 48 && letterToNumberCipher <= 57){
+      }
+      /* Cifra números. Confirma si el numero ASCII esta entre 48 y 57. Completa la formula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a número para luego agregarlo al resultado*/
+      else if(letterToNumberCipher >= 48 && letterToNumberCipher <= 57){
         newNumberCipher = ((letterToNumberCipher - 48 + offset) % 10 + 48);
         newLetterCipher = String.fromCharCode(newNumberCipher);
         resultCipher += newLetterCipher
-      }else if(letterToNumberCipher >= 33 && letterToNumberCipher <= 47){
+      }
+      /* Cifra caracteres especiales. Confirma si el numero ASCII esta entre 33 y 47. Completa la formula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a caracter para luego agregarlo al resultado*/
+      else if(letterToNumberCipher >= 33 && letterToNumberCipher <= 47){
         newNumberCipher = ((letterToNumberCipher - 33 + offset) % 15 + 33);
         newLetterCipher = String.fromCharCode(newNumberCipher);
         resultCipher += newLetterCipher
       }
     }
+    //Retorna el resultado
     return resultCipher;
   },
     
@@ -46,52 +64,83 @@ window.cipher = {
     let substractionResult;
     let resultDecipher="";
 
+    //Recorre la longitud del string dado
     for(let i=0; i<boxToDecipher.length; i++){
 
+      //Verifica si el caracter es ñ o Ñ, y de ser asi, lo agrega al resultado
       if(boxToDecipher === "ñ" || boxToDecipher === "Ñ"){
         resultDecipher += boxToDecipher
       }
+      //Verifica si el offset es negativo, de ser asi, le agrega 26 hasta ser positivo
       while (offset < 0){
         offset = offset + 26
       }
+      //Convierte la letra a número ASCII
       letterToNumberDecipher = boxToDecipher.charCodeAt(i);
+      /*Descifra espacios. Verifica si el número es 32, de ser asi, lo convierte a espacio de nuevo 
+      y lo agrega al resultado*/
       if(letterToNumberDecipher === 32){
         newLetterDecipher = String.fromCharCode(letterToNumberDecipher);
         resultDecipher += newLetterDecipher
-      }else if(letterToNumberDecipher >= 65 && letterToNumberDecipher <= 90){        
+      }
+      /*Descifra mayúsculas. Confirma si el numero ASCII esta entre 65 y 90. Completa la fórmula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a letra para luego agregarlo al resultado*/
+      else if(letterToNumberDecipher >= 65 && letterToNumberDecipher <= 90){
+        //Resta del ASCII menos 65, menos el offset        
         substractionResult = (letterToNumberDecipher - 65 - offset)
+        //Verifica si la resta anterior es menor a 0, de ser asi, suma 26 hasta ser positivo
         while (substractionResult < 0){
           substractionResult = substractionResult + 26
         }
+        //Continuación de fórmula para el descifrado
         newNumberDecipher = substractionResult % 26 + 65
         newLetterDecipher = String.fromCharCode(newNumberDecipher);
         resultDecipher += newLetterDecipher
-      }else if(letterToNumberDecipher >= 97 && letterToNumberDecipher <= 122){
+      }
+      /*Descifra minúsculas. Confirma si el numero ASCII esta entre 97 y 122. Completa la fórmula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a letra para luego agregarlo al resultado*/
+      else if(letterToNumberDecipher >= 97 && letterToNumberDecipher <= 122){
+        //Resta del ASCII menos 97, menos el offset
         substractionResult = (letterToNumberDecipher - 97 - offset)
+        //Verifica si la resta anterior es menor a 0, de ser asi, suma 26 hasta ser positivo
         while (substractionResult < 0){
           substractionResult = substractionResult + 26
         }
+        //Continuación de fórmula para el descifrado
         newNumberDecipher = substractionResult % 26 + 97;
         newLetterDecipher = String.fromCharCode(newNumberDecipher);
         resultDecipher += newLetterDecipher
-      }else if(letterToNumberDecipher >= 48 && letterToNumberDecipher <= 57){
+      }
+      /*Descifra números. Confirma si el numero ASCII esta entre 48 y 57. Completa la fórmula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a número para luego agregarlo al resultado*/
+      else if(letterToNumberDecipher >= 48 && letterToNumberDecipher <= 57){
+        
         substractionResult = (letterToNumberDecipher - 48 - offset);
+        //Verifica si la resta anterior es menor a 0, de ser asi, suma 10 hasta ser positivo
         while (substractionResult < 0){
           substractionResult = substractionResult + 10
         }
+        //Continuación de fórmula para el descifrado
         newNumberDecipher = substractionResult % 10 + 48;
         newLetterDecipher = String.fromCharCode(newNumberDecipher);
         resultDecipher += newLetterDecipher
-      }else if(letterToNumberDecipher >= 33 && letterToNumberDecipher <= 47){
+      }
+      /*Descifra caracteres especiales. Confirma si el numero ASCII esta entre 48 y 57. Completa la fórmula 
+      para obtener el nuevo ASCII y lo convierte de nuevo a caracter para luego agregarlo al resultado*/
+      else if(letterToNumberDecipher >= 33 && letterToNumberDecipher <= 47){
+        //Resta del ASCII menos 33, menos el offset
         substractionResult = (letterToNumberDecipher - 33 - offset);
+        //Verifica si la resta anterior es menor a 0, de ser asi, suma 15 hasta ser positivo
         while (substractionResult < 0){
           substractionResult = substractionResult + 15
         }
+        //Continuación de fórmula para el descifrado
         newNumberDecipher = substractionResult % 15 + 33;
         newLetterDecipher = String.fromCharCode(newNumberDecipher);
         resultDecipher += newLetterDecipher
       }
     }
+    //Retorna el resultado
     return resultDecipher;
   }
 };
